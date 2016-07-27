03f3fd32028171fa97c373c3b4f1520c20f5246b
GiePria <giedre.prialgauskiene@gmail.com>
2016-07-27 13:38:52 +0300

    Forma 105
    Nebenaudoju validate bibliotekos :)

--- registration.js
+++ registration.js
@@ -1,32 +1,32 @@
 $(document).ready(function () {
     //validate registration form on submit
-    $("#registerForm").validate({
-        rules: {
-            username: {
-                required: true,
-                minlength: 3
-            },
-            password: {
-                required: true,
-                minlength: 6
-            },
-            email: {
-                required: true,
-                email: true
-            },
-        },
-        messages: {
-            username: {
-                required: "Please enter a username",
-                minlength: "Your username must consist of at least 3 characters"
-            },
-            password: {
-                required: "Please provide a password",
-                minlength: "Your password must be at least 6 characters long"
-            },
-            email: "Please enter a valid email address",
-        }
-    });
+    /*  $("#registerForm").validate({
+          rules: {
+              username: {
+                  required: true,
+                  minlength: 3
+              },
+              password: {
+                  required: true,
+                  minlength: 6
+              },
+              email: {
+                  required: true,
+                  email: true
+              },
+          },
+          messages: {
+              username: {
+                  required: "Please enter a username",
+                  minlength: "Your username must consist of at least 3 characters"
+              },
+              password: {
+                  required: "Please provide a password",
+                  minlength: "Your password must be at least 6 characters long"
+              },
+              email: "Please enter a valid email address",
+          }
+      });*/
     //...
     $(".button").on('click', function () {
         var email = $('#email').val();
@@ -37,7 +37,11 @@ $(document).ready(function () {
             $(".success").show();
             event.preventDefault();
         } else {
+            $("#ErrorEmail").text("Please enter a valid email address"),
+            $("#ErrorUsername").text("Please enter a username"),
+            $("#ErrorPassword").text("Please provide a password"),
             $("#error").show();
+            event.preventDefault();
         }
     });
 });
